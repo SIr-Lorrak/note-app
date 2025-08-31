113a8ebfabb22b1e813efe2f2fc61766760ae0cc
Sir Lorrak <git.jonathan.bressat@gmail.com>
2025-08-31 21:47:02 +0200

    diagramme en toile affichant les moyennes à l'acceuil

--- front/assets/scripts/main.js
+++ front/assets/scripts/main.js
@@ -17,3 +17,69 @@ for (const m of matieres) {
 }
 
 accueil.style.background = `rgb(${currentState.currentUser.color.data[0]},${currentState.currentUser.color.data[1]},${currentState.currentUser.color.data[2]})`
+
+const ctx = document.getElementById('toile-moyenne');
+const data = {
+  labels: [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    ''
+  ],
+  datasets: [{
+    label: 'Moyenne',
+    data: [65, 59, 90, 81, 56, 55, 40, 100, 34, 12],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+
+const optionsDefault = {
+	responsive: true,
+    elements: {
+      line: {
+        borderWidth: 3
+      }
+    },
+    plugins: {
+    	legend: {
+      		display: false,
+    	}	
+    },
+    scales: {
+        r: {
+            angleLines: {
+                display: true
+            },
+            suggestedMin: 0,
+            suggestedMax: 100,
+	        ticks: {
+				font: {
+				      size: 25
+				    }
+	        }
+        }
+    }
+ }
+
+const config = {
+  type: 'radar',
+  data: data,
+  options: optionsDefault,
+};
+
+const toile = document.getElementById('toile-moyenne');
+
+new Chart(toile, config)
+
