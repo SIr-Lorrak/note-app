d9ae35dd1a8d8c8bc660626c95ccd4782139efe1
Sir Lorrak <git.jonathan.bressat@gmail.com>
2025-08-31 19:12:32 +0200

    page d'accueil, roue des matières

---
+++ front/assets/scripts/data.js
@@ -0,0 +1,218 @@
+//here we'll fetch our data from the api but for now data are mocked
+
+const currentState = {
+	currentUser : {
+		username : "Lorrak",
+		role : 1, // 1 for admin display
+		color : {
+			type : "Buffer",
+			data : [
+				255,
+				0,
+				0
+			]
+		},
+		avatar : {
+			type : "Buffer",
+			data : [
+				0,
+			]
+		},
+		matiere : 6,
+		carton : 0,
+		commentaire : "élève très sage",
+		datecarton : "2000-09-26"
+	},
+	token : "Bearer test",
+	currentPage : "accueil", //current page to show 
+	currentMatiere : "general", // matiere to display on matiere page (0 for general)
+	prec : [], //all the precedent page for the return button (can be a matiere)
+}
+
+const students = [ // student list used for admin
+	{
+		username : "Lorrak",
+		role : 1, // 1 for admin display
+		color : {
+			type : "Buffer",
+			data : [
+				255,
+				0,
+				0
+			]
+		},
+		avatar : {
+			type : "Buffer",
+			data : [
+				0,
+			]
+		},
+		matiere : 6,
+		carton : 0,
+		commentaire : "élève très sage",
+		datecarton : "2000-09-26"
+	},
+	{
+		username : "Titouan Sèchepine",
+		role : 0, // 1 for admin display
+		color : {
+			type : "Buffer",
+			data : [
+				255,
+				255,
+				0
+			]
+		},
+		avatar : {
+			type : "Buffer",
+			data : [
+				0,
+			]
+		},
+		matiere : 1,
+		carton : 1,
+		commentaire : "un peu trop fan de Daniel Balavoine",
+		datecarton : "2025-09-01"
+	},
+	{
+		username : "Martine Du Calpié",
+		role : 0, // 1 for admin display
+		color : {
+			type : "Buffer",
+			data : [
+				128,
+				255,
+				75
+			]
+		},
+		avatar : {
+			type : "Buffer",
+			data : [
+				0,
+			]
+		},
+		matiere : 5,
+		carton : 3,
+		commentaire : "exécrable",
+		datecarton : "2025-09-01"
+	},
+	{
+		username : "Joséphine Pélouze",
+		role : 0, // 1 for admin display
+		color : {
+			type : "Buffer",
+			data : [
+				128,
+				0,
+				255
+			]
+		},
+		avatar : {
+			type : "Buffer",
+			data : [
+				0,
+			]
+		},
+		matiere : 2,
+		carton : 2,
+		commentaire : "attention",
+		datecarton : "2025-09-01"
+	},
+	{
+		username : "Léon Duku",
+		role : 0, // 1 for admin display
+		color : {
+			type : "Buffer",
+			data : [
+				0,
+				0,
+				0
+			]
+		},
+		avatar : {
+			type : "Buffer",
+			data : [
+				0,
+			]
+		},
+		matiere : 4,
+		carton : 3,
+		commentaire : "Banni de twitter (extrèmement raciste)",
+		datecarton : "2025-09-01"
+	},
+]
+
+const note = [ // note list 
+	{
+		id : 1,
+		name : "Titouan Sèchepine",
+		matiere : 1,
+		date : "2025-12-25",
+		notion : "skibidi toilet",
+		note : 76, // en pourcentage
+		note2 : 4, // de NA a A+
+		révision : "Maman",
+		statisfaction : 3
+	},
+	{
+		id : 2,
+		name : "Titouan Sèchepine",
+		matiere : 5,
+		date : "2025-09-11",
+		notion : "les attentats du 11 septembre",
+		note : 99, // en pourcentage
+		note2 : 7, // de NA a A+
+		révision : "Etude",
+		statisfaction : 4
+	},{
+		id : 3,
+		name : "Titouan Sèchepine",
+		matiere : 1,
+		date : "2025-11-11",
+		notion : "pythagore",
+		note : 45, // en pourcentage
+		note2 : 2, // de NA a A+
+		révision : "Papa",
+		statisfaction : 2
+	},{
+		id : 4,
+		name : "Titouan Sèchepine",
+		matiere : 1,
+		date : "2025-12-25",
+		notion : "statistiques",
+		note : 100, // en pourcentage
+		note2 : 7, // de NA a A+
+		révision : "Maman",
+		statisfaction : 5
+	},{
+		id : 5,
+		name : "Titouan Sèchepine",
+		matiere : 1,
+		date : "2025-12-25",
+		notion : "skibidi toilet",
+		note : 76, // en pourcentage
+		note2 : 4, // de NA a A+
+		révision : "Maman",
+		statisfaction : 3
+	},{
+		id : 6,
+		name : "Titouan Sèchepine",
+		matiere : 5 ,
+		date : "2025-12-25",
+		notion : "skibidi toilet",
+		note : 76, // en pourcentage
+		note2 : 4, // de NA a A+
+		révision : "Etude",
+		statisfaction : 3
+	},{
+		id : 7,
+		name : "Titouan Sèchepine",
+		matiere : 1,
+		date : "2025-12-25",
+		notion : "skibidi toilet",
+		note : 76, // en pourcentage
+		note2 : 4, // de NA a A+
+		révision : "Maman",
+		statisfaction : 3
+	},
+]
