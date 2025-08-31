ab04d2a1a7fdff99e4e7107b217a15ffe9141b1e
Sir Lorrak <git.jonathan.bressat@gmail.com>
2025-08-31 19:52:03 +0200

    plus d'animation !!

--- front/assets/scripts/main.js
+++ front/assets/scripts/main.js
@@ -5,6 +5,15 @@ const matiere = document.getElementById("matiere")
 const new_note = document.getElementById("new_note")
 const historique = document.getElementById("historique")
 const statistiques = document.getElementById("statistiques")
+const exercices = document.getElementById("exercices")
 const parametre = document.getElementById("parametre")
+
+const matieres = document.getElementsByClassName("matiere")
+
+for (const m of matieres) {
+	m.onclick = (e) => {
+		console.log(`${m.id} selectionné`)
+	}
+}
 
 accueil.style.background = `rgb(${currentState.currentUser.color.data[0]},${currentState.currentUser.color.data[1]},${currentState.currentUser.color.data[2]})`
