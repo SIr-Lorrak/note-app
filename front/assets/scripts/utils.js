0d3141bc57d0abf401f0c61f79286a9bb8e4d24e
Sir Lorrak <git.jonathan.bressat@gmail.com>
2025-08-31 11:08:09 +0200

    proxy gérer par traefik, plus par nginx

--- front/assets/scripts/utils.js
+++ front/assets/scripts/utils.js
@@ -5,3 +5,10 @@ customElements.define("load-file", class extends HTMLElement {
     this.innerHTML = await (await fetch(src)).text()
   }
 })
+
+Date.prototype.getWeek = function() {
+  var onejan = new Date(this.getFullYear(), 0, 1);
+  var millisecsInDay = 86400000;
+  const weekDay = Math.ceil((((this - onejan) / millisecsInDay) + onejan.getDay()) / 7);
+  return weekDay === 53 ? 1 : weekDay
+};
